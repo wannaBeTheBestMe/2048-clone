6e0e60322611421576fb2357557a46e731366212
Agastya Makkar <agastya.makkar@gmail.com>
2021-05-31 13:58:39 +0530

    .

--- sketch.js
+++ sketch.js
@@ -1,6 +1,11 @@
+/*
+1. An update in the grid values needs to cause an update on screen.
+*/
+
 let fontRegular, fontItalic, fontBold;
 let grid;
 let tiles = [];
+let size = 100;
 
 function preload() {
   fontRegular = loadFont("Assets/ClearSans-Regular.ttf");
@@ -10,200 +15,145 @@ function preload() {
 
 function setup() {
   createCanvas(800, 800);
+  background(Colors.BACKGROUND);
   grid = new Grid(4, 4);
   for (let i = 0; i < 2; i++) {
-    tiles[i] = new Grid.Tile(0, 0);
+    let randomValue = random([2, 4]);
+    let randomColor = randomValue == 2 ? Colors._2 : Colors._4;
+    tiles[i] = new Grid.Tile(0, 0, randomValue, randomColor);
   }
+  grid.spawn(2);
 }
 
 function draw() {
-  background(205, 193, 180, 255);
-  for (let i = 0; i < tiles.length; i++) {
-    tiles[i].spawn();
-    tiles[i].displayValue();
-  }
   grid.display();
-  grid.update();
-  noLoop();
 }
 
 class Grid {
-  constructor(length, breadth) {
-    this.length = length;
+  constructor(_length, breadth) {
+    this._length = _length;
     this.breadth = breadth;
-    this.widthConstant = width / this.length;
+    this.widthConstant = width / this._length;
     this.heightConstant = height / this.breadth;
     this.weight = 20;
     this.grid = [];
+    for (let i = 0; i < this._length; i++) {
+      this.grid[i] = new Array(this.breadth).fill(0);
+    }
   }
 
   display() {
-    for (let i = 0; i < this.length; i++) {
-      this.grid[i] = new Array(this.breadth).fill(0);
-    }
-
-    let positions = [
-      {
-        x1: 0,
-        y1: this.weight / 2,
-        x2: width,
-        y2: this.weight / 2,
-      },
-      {
-        x1: width - this.weight / 2,
-        y1: 0,
-        x2: width - this.weight / 2,
-        y2: height,
-      },
-      {
-        x1: width,
-        y1: height - this.weight / 2,
-        x2: 0,
-        y2: height - this.weight / 2,
-      },
-      {
-        x1: this.weight / 2,
-        y1: height,
-        x2: this.weight / 2,
-        y2: 0,
-      },
-      {
-        x1: this.widthConstant,
-        y1: height,
-        x2: this.widthConstant,
-        y2: 0,
-      },
-      {
-        x1: 2 * this.widthConstant,
-        y1: height,
-        x2: 2 * this.widthConstant,
-        y2: 0,
-      },
-      {
-        x1: 3 * this.widthConstant,
-        y1: height,
-        x2: 3 * this.widthConstant,
-        y2: 0,
-      },
-      {
-        x1: 0,
-        y1: this.heightConstant,
-        x2: width,
-        y2: this.heightConstant,
-      },
-      {
-        x1: 0,
-        y1: 2 * this.heightConstant,
-        x2: width,
-        y2: 2 * this.heightConstant,
-      },
-      {
-        x1: 0,
-        y1: 3 * this.heightConstant,
-        x2: width,
-        y2: 3 * this.heightConstant,
-      },
-    ];
-
-    for (let position of positions) {
-      strokeWeight(this.weight);
+    for (let j = 0; j < 5; j++) {
       stroke(187, 173, 160, 255);
-      line(position.x1, position.y1, position.x2, position.y2);
+      j === 0 || j === 4 ? strokeWeight(2 * this.weight) : strokeWeight(this.weight);
+
+      line(0, this.heightConstant * j, width, this.heightConstant * j);
+      line(this.widthConstant * j, 0, this.widthConstant * j, height);
     }
   }
 
-  update() {
-    for (let i = 0; i < tiles.length; i++) {
-      let xCoordinateTile = Object.keys(tiles[i].x)[0];
-      let yCoordinateTile = Object.keys(tiles[i].y)[0];
-      this.grid[xCoordinateTile][yCoordinateTile] = tiles[i].value;
+  spawn(numberOfTiles) {
+    for (let i = 0; i < numberOfTiles; i++) {
+      let possibleXValues = [0, 1, 2, 3];
+      let possibleYValues = [0, 1, 2, 3];
+
+      if (tiles.length > 1) {
+        let lastTile = tiles[tiles.length - 1];
+        let randomNumber = random();
+        console.log(randomNumber);
+
+        randomNumber > 0.5 ? possibleXValues.splice(lastTile.x, 1) : possibleXValues.splice(lastTile.y, 1);
+      }
+
+      console.log([possibleXValues, possibleYValues]);
+
+      let xValue = random(possibleXValues);
+      let yValue = random(possibleYValues);
+
+      this.grid[xValue][yValue] = 2;
+
+      tiles[i].spawn(xValue, yValue);
     }
   }
 }
 
 Grid.Tile = class {
   constructor(x, y, value, color) {
-    let possibleXValues = [
-      { 0: grid.weight },
-      { 1: grid.widthConstant + 0.5 * grid.weight },
-      { 2: 2 * grid.widthConstant + 0.5 * grid.weight },
-      { 3: 3 * grid.widthConstant + 0.5 * grid.weight },
-    ];
-    let possibleYValues = [
-      { 0: 3 * grid.heightConstant + 0.5 * grid.weight },
-      { 1: 2 * grid.heightConstant + 0.5 * grid.weight },
-      { 2: grid.heightConstant + 0.5 * grid.weight },
-      { 3: grid.weight },
-    ];
-
-    if (tiles.length >= 2) {
-      let lastTile = tiles[tiles.length - 1];
-      let randomNumber = random();
-      if (randomNumber > 0.5) {
-        let lastTileCoordinate = parseFloat(Object.keys(lastTile.x)[0]);
-        possibleXValues.splice(lastTileCoordinate, 1);
-      } else {
-        let lastTileCoordinate = parseFloat(Object.keys(lastTile.y)[0]);
-        possibleYValues.splice(lastTileCoordinate, 1);
-      }
-    }
-
-    console.log(possibleXValues);
-    console.log(possibleYValues);
-
-    this.x = random(possibleXValues);
-    this.y = random(possibleYValues);
-
-    this.value = random([2, 4]);
-    this.color = this.value == 2 ? "RGB(238, 228, 218)" : "RGB(238, 225, 201)";
+    this.x = x;
+    this.y = y;
+    this.value = value;
+    this.color = color;
   }
 
-  spawn() {
+  spawn(xPos, yPos) {
+    let possibleXValues = [
+      grid.weight,
+      grid.widthConstant + 0.5 * grid.weight,
+      2 * grid.widthConstant + 0.5 * grid.weight,
+      3 * grid.widthConstant + 0.5 * grid.weight
+    ];
+    let possibleYValues = [
+      3 * grid.heightConstant + 0.5 * grid.weight,
+      2 * grid.heightConstant + 0.5 * grid.weight,
+      grid.heightConstant + 0.5 * grid.weight,
+      grid.weight
+    ];
+
+    this.x = possibleXValues[xPos];
+    this.y = possibleYValues[yPos];
+
     noStroke();
     fill(this.color);
     let tileWidth = grid.widthConstant - 0.5 * grid.weight;
     let tileHeight = grid.heightConstant - 0.5 * grid.weight;
     rect(
-      Object.values(this.x)[0],
-      Object.values(this.y)[0],
+      this.x,
+      this.y,
       tileWidth,
       tileHeight
     );
+    this.displayValue(
+      this.value,
+      this.x + (grid.widthConstant - 1.5 * grid.weight) / 2 - size / 4,
+      this.y + (grid.heightConstant - 1.5 * grid.weight) / 2 + size / 3
+    );
   }
 
-  displayValue() {
+  displayValue(textMessage, textX, textY) {
     noStroke();
-    fill(119, 110, 101);
-    let size = 100;
+    fill(Colors.TEXT);
     textSize(size);
     textFont(fontBold);
-    text(
-      this.value,
-      Object.values(this.x)[0] +
-        (grid.widthConstant - 1.5 * grid.weight) / 2 -
-        size / 4,
-      Object.values(this.y)[0] +
-        (grid.heightConstant - 1.5 * grid.weight) / 2 +
-        size / 3
-    );
+    text(textMessage, textX, textY);
+  }
 
-    // strokeWeight(5);
-    // stroke(51);
-    // point(this.x + (grid.widthConstant - 1.5 * grid.weight) / 2, this.y + (grid.heightConstant - 1.5 * grid.weight) / 2)
+  updatePositions() {
+    // switch (keyCode) {
+    //   case RIGHT_ARROW:
+    //     break;
+    // }
   }
 };
 
-/*
-Color Codes
-  2: 238,228,218,255
-  4: 238,225,201,255
-  8: 243,178,122,255
-  16: 246,150,100,255
-  32: 247,124,95,255
-  64: 247,95,59,255
-  128: 237,208,115,255
-  256: 236,203,101,255
-  512: 236,200,90,255
-  1024: 231,194,87,255
-  2048: 232,190,78,255
- */
+function keyPressed() {
+  for (let i = 0; i < tiles.length; i++) {
+    tiles[i].updatePositions();
+  }
+}
+
+class Colors {
+  static BACKGROUND = "RGBA(205, 193, 180, 1)";
+  static LINES = "RGBA(187, 173, 160, 1)";
+  static TEXT = "RGBA(119, 110, 101, 1)";
+  static _2 = "RGBA(238, 228, 218, 1)";
+  static _4 = "RGBA(238, 225, 201, 1)";
+  static _8 = "RGBA(243, 178, 122, 1)";
+  static _16 = "RGBA(246, 150, 100, 1)";
+  static _32 = "RGBA(247, 124, 95, 1)";
+  static _64 = "RGBA(247, 95, 59, 1)";
+  static _128 = "RGBA(237, 208, 115, 1)";
+  static _256 = "RGBA(236, 203, 101, 1)";
+  static _512 = "RGBA(236, 200, 90, 1)";
+  static _1024 = "RGBA(231, 194, 87, 1)";
+  static _2048 = "RGBA(232, 190, 78, 1)";
+}
